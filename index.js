4836bdf865555541a15b2f27517f5b6cbca6f10c
JcElectronic <61955883+johancass@users.noreply.github.com>
2025-07-30 03:54:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const generarPagoRouter = require("./genrar_pago");
+const generarPagoRouter = require("./Generar_pago");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
