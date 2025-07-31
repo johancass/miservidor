a2cb05c0281e131cac5de0574179bdfa15cf1e19
JcElectronic <61955883+johancass@users.noreply.github.com>
2025-07-31 01:15:03 -0500

    Update generar_pago.php

--- generar_pago.php
+++ generar_pago.php
@@ -1,39 +1,32 @@
 <?php
-// Configura tus credenciales de PayU aquí
+header('Content-Type: application/json');
+
+// Parámetros de PayU
 $apiKey = "TU_API_KEY";
 $merchantId = "TU_MERCHANT_ID";
 $accountId = "TU_ACCOUNT_ID";
-$referenceCode = isset($_GET['id']) ? $_GET['id'] : 'pedido123';
-$amount = "10000"; // Monto en COP
+
+// Recibir el ID del pedido
+$referenceCode = isset($_GET['id']) ? $_GET['id'] : '';
+if ($referenceCode == '') {
+    echo json_encode(["status" => "error", "message" => "ID no proporcionado"]);
+    exit;
+}
+
+$amount = "10000";
 $currency = "COP";
 $description = "Pago generado desde ESP";
 
-// Firma digital (según documentación de PayU)
+// Generar firma
 $signature = md5($apiKey . "~" . $merchantId . "~" . $referenceCode . "~" . $amount . "~" . $currency);
 
-// Datos para la solicitud
-$data = [
-    "merchantId" => $merchantId,
-    "accountId" => $accountId,
-    "description" => $description,
-    "referenceCode" => $referenceCode,
-    "amount" => $amount,
-    "currency" => $currency,
-    "signature" => $signature,
-    "test" => "1", // Cambiar a 0 en producción
-    "buyerEmail" => "comprador@email.com",
-    "responseUrl" => "https://tuweb.com/response",
-    "confirmationUrl" => "https://tuweb.com/confirmacion"
-];
+// Construir URL de PayU con parámetros
+$link_pago = "https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu?merchantId={$merchantId}&accountId={$accountId}&description={$description}&referenceCode={$referenceCode}&amount={$amount}&currency={$currency}&signature={$signature}&test=1&buyerEmail=cliente@email.com&responseUrl=https://tuweb.com/response&confirmationUrl=https://tuweb.com/confirmacion";
 
-// Construir formulario automático
-$form = '<form id="payuForm" method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway/">
-';
-foreach ($data as $name => $value) {
-    $form .= '<input name="' . $name . '" type="hidden" value="' . $value . '">' . "\n";
-}
-$form .= '</form><script>document.getElementById("payuForm").submit();</script>';
-
-// Imprimir el HTML que redirige automáticamente
-echo $form;
+// Responder con el link en JSON
+echo json_encode([
+    "status" => "ok",
+    "id" => $referenceCode,
+    "link" => $link_pago
+]);
 ?>
